3f4a04abddb7e38588cef397bd8ee981dc437e64
Nurmdau <narymbaev.n02@gmail.com>
2025-01-03 19:00:59 +0500

    dependencies

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import user, post, comment
+from .routers import user # , post, comment
 from .database import engine
 from . import models
 
@@ -8,5 +8,5 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Blog Platform API")
 
 app.include_router(user.router)
-app.include_router(post.router)
-app.include_router(comment.router)
+# app.include_router(post.router)
+# app.include_router(comment.router)
