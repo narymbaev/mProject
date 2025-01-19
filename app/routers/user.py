0499473d5920812e4cfe20c20be76cb0f99590fe
Nurmdau <narymbaev.n02@gmail.com>
2025-01-19 13:12:19 +0500

    user create

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,20 +1,52 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from typing import List
+
+from ..utils import hash_password
 from ..database import get_db
 from ..models import user as models
 from ..schemas import user as schemas
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.post("/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email == user.email).first()
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return db_user
 
 @router.get("/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = db.query(models.User).offset(skip).limit(limit).all()
     return users
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    db_user = db.query(models.User).filter(
+        or_(
+            models.User.email == user.email,
+            models.User.username == user.username,
+        )
+    ).first()
+    if db_user:
+        if db_user.email == user.email:
+            raise HTTPException(status_code=400, detail="Email already registered")
+        else:
+            raise HTTPException(status_code=400, detail="Username already registered")
+
+    hashed_password = hash_password(user.password)
+
+    new_user = models.User(
+        email=user.email,
+        username=user.username,
+        hashed_password=hashed_password
+    )
+
+    # add user
+    db.add(new_user)
+
+    # commit to save it
+    db.commit()
+
+    # get generated ID
+    db.refresh(new_user)
+
+    return new_user
+
